fca9d20cb81e0087d7cbe72212b4a42962d79e95
Vitor Mukai <vitorslmuk@gmail.com>
2018-05-18 00:17:45 -0300

    Iniciando projeto de treino Spring e build inicial

---
+++ src/main/java/br/com/vmukai/springtraining/SpringtrainingApplication.java
@@ -0,0 +1,12 @@
+package br.com.vmukai.springtraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringtrainingApplication.class, args);
+	}
+}
